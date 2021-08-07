f63d88c26d9f1556668906cac5decd22b6cf8c4d
Dan <DanielMooreRealPerson@gmail.com>
2021-08-07 00:28:27 -0400

    commit tree 88622f7fdbfce219f3aa5a1a00ff04b9db38c400

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const record = [
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" },
+    //...
+  ]
 
+function superbowlWin(record) {
+    let winningYear = record.find((y) => y.result === "W")
+    if (winningYear) {
+        return winningYear.year
+    }
+    return undefined
+}
+superbowlWin(record)
